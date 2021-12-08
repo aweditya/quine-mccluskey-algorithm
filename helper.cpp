00899311f3c69cf0a1a1fd6a6f750a7c2ac7de45
Aditya Sriram <aweditya@gmail.com>
2021-12-08 21:44:21 +0530

    First grouping stage almost done. Needs some debugging for edge cases

--- helper.cpp
+++ helper.cpp
@@ -5,9 +5,9 @@
 /**
  * @brief Return the number of ones in the binary representation
  * of an integer using Brian-Kernighan's algorithm
- * 
- * @param x 
- * @return int 
+ *
+ * @param x
+ * @return int
  */
 int countOnesInBinaryRepresentation(int x)
 {
@@ -28,19 +28,51 @@ int countOnesInBinaryRepresentation(int x)
 }
 
 /**
- * @brief Compute the Hamming distance between two character
- * sequences of the same length
- * 
- * @param a 
- * @param b 
- * @param n 
- * @return int 
+ * @brief Convert an integer into its binary representation
+ * of fixed length n
+ *
+ * @param x
+ * @param n
+ * @param fixedWidthBinaryRepresentation
  */
-int computeHammingDistance(char* a, char* b, int n)
+std::string convertToBinary(int x, int n)
+{
+    std::string fixedWidthBinaryRepresentation = "";
+    for (int i = 0; i < n; i++)
+    {
+        if (x != 0)
+        {
+            if (x % 2 == 1)
+            {
+                fixedWidthBinaryRepresentation = '1' + fixedWidthBinaryRepresentation;
+            }
+            else
+            {
+                fixedWidthBinaryRepresentation = '0' + fixedWidthBinaryRepresentation;
+            }
+            x /= 2;
+        }
+        else
+        {
+            fixedWidthBinaryRepresentation = '0' + fixedWidthBinaryRepresentation;
+        }
+    }
+    return fixedWidthBinaryRepresentation;
+}
+
+/**
+ * @brief Compute the Hamming distance between two strings
+ * of the same length
+ *
+ * @param a
+ * @param b
+ * @param n
+ * @return int
+ */
+int computeHammingDistance(std::string a, std::string b)
 {
     int hammingDistance = 0;
-    std::cout << a << "\t" << b << "\n";
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < a.length(); i++)
     {
         if (a[i] != b[i])
         {
@@ -51,103 +83,87 @@ int computeHammingDistance(char* a, char* b, int n)
 }
 
 /**
- * @brief Convert an integer into its binary representation
- * of fixed length n
- * 
- * @param x 
- * @param n 
- * @param fixedWidthBinaryRepresentation 
- */
-void convertToBinary(int x, int n, char fixedWidthBinaryRepresentation[])
-{
-    for (int i = n - 1; i >= 0; i--)
-    {
-        if (x != 0)
-        {
-            if (x % 2 == 1)
-            {
-                fixedWidthBinaryRepresentation[i] = '1';
-            }
-            else 
-            {
-                fixedWidthBinaryRepresentation[i] = '0';
-            }
-            x /= 2;
-        }
-        else 
-        {
-            fixedWidthBinaryRepresentation[i] = '0';
-        }
-    }
-}
-
-/**
- * @brief Find the position at which two character 
- * sequences differ. This intended use of this 
- * function is with bit strings differing by a 
- * single character. If used in general, it will 
+ * @brief Find the position at which two character
+ * sequences differ. This intended use of this
+ * function is with bit strings differing by a
+ * single character. If used in general, it will
  * return the first position of difference
- * 
- * @param a 
- * @param b 
- * @return int 
+ *
+ * @param a
+ * @param b
+ * @return int
  */
-int positionOfDifference(char* a, char* b)
+int findPositionOfDifference(std::string a, std::string b)
 {
     int index = 0;
-    while(a[index] == b[index])
+    while (a[index] == b[index])
     {
         index++;
     }
     return index;
 }
 
-std::map<std::string, std::vector<int> > groupImplicants(std::vector <int> groups[], int n)
+void groupImplicants(std::vector<int> groups[], int n)
 {
-    std::map<std::string, std::vector<int> > groupedImplicants;
-    return groupedImplicants;
-}
-
-int main(int argc, char** argv)
-{
-    int n = 4;
-    int minterms[] = {4, 8, 9, 10, 11, 12, 14, 15};
-
-    std::cout << "Minterms: ";
-    for (int i = 0; i < 8; i++)
+    std::vector<std::string> binary;
+    std::vector<std::vector<int>> implicants;
+    // groups has 5 rows: 0, 1, 2, 3, 4
+    for (int i = 0; i < n; i++)
     {
-        std::cout << minterms[i] << " ";
+        for (auto x : groups[i])
+        {
+            bool used = false;
+            for (auto y : groups[i + 1])
+            {
+                std::string xBinary = convertToBinary(x, n);
+                std::string yBinary = convertToBinary(y, n);
+                int hamming = computeHammingDistance(xBinary, yBinary);
+                if (hamming == 1)
+                {
+                    int index = findPositionOfDifference(xBinary, yBinary);
+                    std::string key = xBinary;
+                    key[index] = '-';
+                    binary.push_back(key);
+                    std::vector<int> pair;
+                    pair.push_back(x);
+                    pair.push_back(y);
+                    implicants.push_back(pair);
+
+                    used = true;
+                }
+            }
+            if (!used)
+            {
+                binary.push_back(convertToBinary(x, n));
+                std::vector<int> implicant(1, x); 
+                implicants.push_back(implicant);
+            }
+        }
     }
-    std::cout << "\n";
 
-    std::cout << "Testing convertToBinary():\n";
-    char binaryOf4[5], binaryOf6[5];
-    convertToBinary(4, 4, binaryOf4);
-    convertToBinary(6, 4, binaryOf6);
-    std::cout << "Binary representation of 4: " << binaryOf4 << "\n";
-
-    std::cout << "Testing computeHammingDistance()\n";
-    std::cout << "Hamming distance between 4 and 6: " << computeHammingDistance(binaryOf4, binaryOf6, 4) << "\n";
-
-    std::cout << "Testing positionOfDifference()\n";
-    std::cout << "Position of difference for 4 and 6: " << positionOfDifference(binaryOf4, binaryOf6) << "\n";
-
-    std::cout << "Testing grouping by number of ones in binary representation:\n";
-    std::vector<int> groupedByOnes[5];
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < binary.size(); i++)
     {
-        int ones = countOnesInBinaryRepresentation(minterms[i]);
-        groupedByOnes[ones].push_back(minterms[i]);
-    }
-
-    std::cout << "Grouping minterms by number of ones in bitstring:\n";
-    for (int i = 0; i <= n; i++)
-    {
-        std::cout << i << ": ";
-        for (auto x : groupedByOnes[i])
+        std::cout << binary[i] << ": ";
+        for (auto x : implicants[i])
         {
             std::cout << x << " ";
         }
         std::cout << "\n";
     }
+}
+
+int main(int argc, char **argv)
+{
+    int n = 4, mintermCount = 8;
+    int minterms[] = {4, 8, 9, 10, 11, 12, 14, 15};
+    // int minterms[] = {0, 3, 4, 15};
+
+    std::vector<int> groupedByOnes[5];
+    for (int i = 0; i < mintermCount; i++)
+    {
+        int ones = countOnesInBinaryRepresentation(minterms[i]);
+        groupedByOnes[ones].push_back(minterms[i]);
+    }
+
+    groupImplicants(groupedByOnes, n);
 }
